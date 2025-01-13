c3741af0b6c508eaa22972e3860b2aa3d1b8884e
Andrea Scasso <andrea@scasso.com>
2025-01-12 23:34:34 -0800

    Sample code.

---
+++ sample/src/test/java/io/github/thymeleaf/assetdialect/sample/SampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.thymeleaf.assetdialect.sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
